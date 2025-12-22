1a89e784ef76d9d7321d4b238093362cbf6ac5bc
Tracy Atteberry <tracy@magicbydesign.com>
2025-12-22 12:55:42 -0600

    test: add edge cases for employer slugification

--- test/employer_test.rb
+++ test/employer_test.rb
@@ -6,4 +6,19 @@ describe Jojo::Employer do
     employer = Jojo::Employer.new('Acme Corp')
     _(employer.slug).must_equal 'acme-corp'
   end
+
+  it "slugifies special characters" do
+    employer = Jojo::Employer.new('AT&T Inc.')
+    _(employer.slug).must_equal 'at-t-inc'
+  end
+
+  it "slugifies multiple spaces" do
+    employer = Jojo::Employer.new('Example  Company   LLC')
+    _(employer.slug).must_equal 'example-company-llc'
+  end
+
+  it "slugifies leading and trailing special characters" do
+    employer = Jojo::Employer.new('!Company!')
+    _(employer.slug).must_equal 'company'
+  end
 end
